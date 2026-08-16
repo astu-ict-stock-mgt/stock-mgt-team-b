cbf7a6329da6b89f3bedec502ded1bca62a10263
Lidetu <lidosinafanis@gmail.com>
2026-08-16 14:04:40 +0300

    feat(routes): plug inventory module routing architecture into application core

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import authRoutes from '../modules/auth/routes.ts';
+import inventoryRoutes from '../modules/inventory/routes.ts';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
+router.use('/inventory', inventoryRoutes);
 
 export default router;
